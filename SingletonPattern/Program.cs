966bd4bed4d14c0627a5708dced4cf1e5d1acbea
edopolimac <edopolimac@gmail.com>
2015-03-30 21:51:54 +0200

    Added code

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = Logger.GetInstance();
+
+            logger.FriteToFile();
+
+            Console.ReadLine();
+        }
+    }
+}
